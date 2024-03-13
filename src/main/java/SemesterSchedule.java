1d91c864d50bc5ba75a266300964a2369c9ae89d
dzeilstra <zeilstraac20@gcc.edu>
2024-03-13 14:31:12 -0400

    Fixing conflicts

--- src/main/java/SemesterSchedule.java
+++ src/main/java/SemesterSchedule.java
@@ -1,15 +1,19 @@
 import java.util.ArrayList;
 
 public class SemesterSchedule extends CourseList {
-    private ArrayList<Course> courses; //ELliott is here
+    private ArrayList<Course> courses;
     private Boolean isFall;
 
-    public SemesterSchedule(){
-        //we'll probably want a constructor that loads from a file.
-    }
-
     public void addCourse(Course course) {
-        courses.add(course);
+        boolean overlapsWith = false;
+        for (int i = 0; i < courses.size(); i++) {
+            if (course.overlapsWith(courses.get(i))) {
+                overlapsWith = true;
+            }
+        }
+        if (!overlapsWith) {
+            courses.add(course);
+        }
     }
 
     @Override
