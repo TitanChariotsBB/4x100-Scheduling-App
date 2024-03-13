5bb1a463418a8a19bde44f2d50c34761a5d73722
dzeilstra <zeilstraac20@gcc.edu>
2024-03-12 22:47:34 -0400

    Added comment (yeah)

--- src/main/java/SemesterSchedule.java
+++ src/main/java/SemesterSchedule.java
@@ -9,15 +9,7 @@ public class SemesterSchedule extends CourseList {
     }
 
     public void addCourse(Course course) {
-        boolean overlapsWith = false;
-        for (int i = 0; i < courses.size(); i++) {
-            if (course.overlapsWith(courses.get(i))) {
-                overlapsWith = true;
-            }
-        }
-        if (!overlapsWith) {
-            courses.add(course);
-        }
+        // Checks for conflict
     }
 
     @Override
