c4eb95c57e428cd1724bd7af6add9bc38777e004
NStriebel <StriebelNJ21@gcc.edu>
2024-03-20 19:27:57 -0400

    Added Working Code for loading a CourseList

--- src/test/java/FileHandlerTest.java
+++ src/test/java/FileHandlerTest.java
@@ -4,6 +4,13 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class FileHandlerTest {
     @Test
+    void testLoad(){
+        CourseList catalog = FileHandler.loadCatalog();
+        FileHandler.saveList(catalog, "test-save");
+        CourseList loaded = FileHandler.loadList("test-save");
+    }
+
+    @Test
     void testSave(){
         CourseList catalog = FileHandler.loadCatalog();
         FileHandler.saveList(catalog, "test-save");
