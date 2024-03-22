c0ce03363c873e6ec9175fb2b81461e0b67242e8
TitanChariotsBB <cmagwoodva@gmail.com>
2024-03-21 22:41:37 -0400

    Improved Controller script

--- src/main/java/org/example/FXMLController.java
+++ src/main/java/org/example/FXMLController.java
@@ -1,9 +1,7 @@
 package org.example;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.control.TabPane;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.VBox;
@@ -11,15 +9,34 @@ import javafx.scene.layout.VBox;
 public class FXMLController {
     @FXML
     private Label debugLabel;
-
     @FXML
     private TabPane tabPane;
-
     @FXML
     private VBox searchResults;
-
     @FXML
     private TextField searchBar;
+    @FXML
+    private Accordion filterAccordion;
+    @FXML
+    private ComboBox<String> dptComboBox;
+    @FXML
+    private ComboBox<String> mtgDaysComboBox;
+    @FXML
+    private ComboBox<String> startTimeComboBox;
+    @FXML
+    private TextField courseNumber;
+    @FXML
+    private TextField courseName;
+    @FXML
+    private TextField professor;
+
+    @FXML
+    public void initialize() {
+        ControllerHelper ch = new ControllerHelper();
+        dptComboBox.getItems().setAll(ch.dptOptions);
+        mtgDaysComboBox.getItems().setAll(ch.dayOptions);
+        startTimeComboBox.getItems().setAll(ch.timeOptions);
+    }
 
     @FXML
     protected void onSearchButtonClick() {
@@ -32,5 +49,21 @@ public class FXMLController {
     protected void onKeyPressed(KeyEvent ke) {
         if (ke.getCode() == KeyCode.ENTER)
             onSearchButtonClick();
+    }
+
+    @FXML
+    protected void onClearFiltersButtonClicked() {
+        courseNumber.setText("");
+        courseName.setText("");
+        professor.setText("");
+        dptComboBox.getSelectionModel().clearSelection();
+        mtgDaysComboBox.getSelectionModel().clearSelection();
+        startTimeComboBox.getSelectionModel().clearSelection();
+        // TODO: call remove filter
+    }
+
+    @FXML
+    protected void onApplyFiltersButtonClicked() {
+
     }
 }
