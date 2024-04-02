a451cb2fb4996a60a67df707d03dd2228893607d
TitanChariotsBB <cmagwoodva@gmail.com>
2024-04-02 16:56:26 -0400

    Links Alex's search to the UI

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -17,24 +17,24 @@ public class Main {
 
     public static void main(String[] args) {
         // I am aware most of this stuff should go in run eventually. This is just for testing
-        System.out.println("Hello!");
-        CourseList cs = new CourseList();
-        search = new Search(cs);
-
         LocalDateTime time1 = LocalDateTime.of(2024, 1, 1, 9, 0);
         LocalDateTime[][] meetings1 = {{time1, time1}, null, {time1, time1}, null, {time1, time1}};
         Course course1 = new Course("Underwater basket weaving", "HUMA 201", meetings1,true, "A good class", "STEM 376", "Dr. Bibza", 3, null);
         LocalDateTime time2 = LocalDateTime.of(2024, 1, 1, 14, 0);
         LocalDateTime[][] meetings2 = {null, {time2, time2}, null, {time2, time2}, null};
         Course course2 = new Course("Foundations of balloon fabrication", "HUMA 301", meetings2,true, "A better class", "HAL 116", "Dr. Bibza", 3, null);
+
+        catalog = new CourseList();
+        catalog.addCourse(course1);
+        catalog.addCourse(course2);
+
         fallSemester = new CourseList();
         fallSemester.addCourse(course1);
         fallSemester.addCourse(course2);
         springSemester = new CourseList();
-        past = new CourseList();
         future = new CourseList();
-        past.addCourse(course1);
-        future.addCourse(course2);
+        past = new CourseList();
+        search = new Search(catalog);
 
         MainApp.launchGUI();
     }
