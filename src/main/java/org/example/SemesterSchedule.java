71e191fe70bcf5847b311c354eeac6495398fd0f
NStriebel <StriebelNJ21@gcc.edu>
2024-04-03 10:36:25 -0400

    working patch for error loading catalog

--- src/main/java/org/example/SemesterSchedule.java
+++ src/main/java/org/example/SemesterSchedule.java
@@ -4,31 +4,34 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 public class SemesterSchedule extends CourseList {
-    private ArrayList<Course> courses;
     private Boolean isFall;
 
-    public SemesterSchedule(){
-        courses = new ArrayList<>();
+    public SemesterSchedule(Boolean isFall){
+        super();
+        this.isFall = isFall;
         // we'll probably want a constructor that loads from a file.
     }
 
-    public void addCourse(Course course) {
-        boolean overlapsWith = false;
-        if (courses != null) {
-            for (Course cours : courses) {
-                if (course.overlapsWith(cours)) {
-                    overlapsWith = true;
-                    System.out.println("Error: Overlaps with course " + cours.getName());
-                }
-            }
-            if (!overlapsWith) {
-                courses.add(course);
+    public SemesterSchedule(CourseList listToConvert, Boolean isFall){
+        super();
+        this.isFall = isFall;
+
+        for(Course course : listToConvert.getCourses()){
+            this.addCourse(course);
+        }
+    }
+
+    @Override
+    public void addCourse(Course course) throws IllegalArgumentException {
+        for (Course existingCourse : super.getCourses()) {
+            if (course.overlapsWith(existingCourse)) {
+                throw new IllegalArgumentException("Attempted to add a course overlapping with " + existingCourse);
             }
         }
-        else {
-            courses = new ArrayList<>();
-            courses.add(course);
+        if(super.getTotalCredits() + course.getCredits() > 19) {
+            throw new IllegalArgumentException("Attempted to add too many credits");
         }
+        super.addCourse(course);//adds course to the arrayList and increments totalCredits
     }
 
     @Override
@@ -38,7 +41,7 @@ public class SemesterSchedule extends CourseList {
 
         String[] rows = {"\n", "\n", "\n", "\n", "\n", "\n", "\n", "\n", "\n", "\n", "\n", "\n"};
 
-        for (Course course : courses) {
+        for (Course course : super.getCourses()) {
             String code = course.getCode();
             int timeSliceIdx = 0;
             String timeSlice = "";
