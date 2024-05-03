ffdd2eba00c16b0a0cfed3e9cbe0a80ae03568f4
TitanChariotsBB <cmagwoodva@gmail.com>
2024-05-03 10:14:04 -0400

    The previous commit fixes the timeslot bug

--- src/main/java/org/example/Course.java
+++ src/main/java/org/example/Course.java
@@ -143,7 +143,6 @@ public class Course {
     }
 
     public ArrayList<String> getPrerequisites() {
-        //ArrayList<Course> prereqs = new ArrayList<>();
         return prerequisites;
     }
 
